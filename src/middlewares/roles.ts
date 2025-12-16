f156990c8442f61b85681e1b210660a0de0c1453
AxelLaraG <axmadlar@gmail.com>
2025-12-16 17:47:10 -0600

    Permisos por roles agregado

--- src/middlewares/roles.ts
+++ src/middlewares/roles.ts
@@ -1,22 +1,12 @@
 import { Response, NextFunction } from "express";
 import { RequestUsuario } from "./auth";
 
-export const esAdmin = (
-  req: RequestUsuario,
-  res: Response,
-  next: NextFunction,
-): void => {
-  if (!req.user) {
-    res.status(500).json({ message: "Error de servidor: Rol sin token" });
-    return;
-  }
-
-  const { role } = req.user;
-
-  if (role !== 1) {
-    res.status(403).json({ message: "Acceso denegado" });
-    return;
-  }
-  
-  next();
+export const permitirRoles = (...rolesPermitidos: number[]) => {
+  return (req: RequestUsuario, res: Response, next: NextFunction) => {
+    if (!req.user || !rolesPermitidos.includes(req.user.role)) {
+      res.status(403).json({ message: "No tienes permisos" });
+      return;
+    }
+    next();
+  };
 };
--- src/routes/usuariosRoutes.ts
+++ src/routes/usuariosRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { crearUsuario, obtenerUsuariosPorRol } from "../controllers/usuarioController"
 import { validarToken } from "../middlewares/auth";
-import { esAdmin } from "../middlewares/roles";
+import { permitirRoles } from "../middlewares/roles";
 
 const router = Router();
 
-router.get('/getUsrsByRole', validarToken, esAdmin, obtenerUsuariosPorRol);
-router.post('/createUsr', validarToken, esAdmin, crearUsuario);
+router.get('/getUsrsByRole', validarToken, permitirRoles(1), obtenerUsuariosPorRol);
+router.post('/createUsr', validarToken, permitirRoles(1), crearUsuario);
 
 export default router;
